553c8fd0620c0de744eb37660308885d65d2ee63
Oscar Checa <ochecaa21@gmail.com>
2025-10-06 16:45:12 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    // Más adelante conectaremos con tu backend
+    setData("<p>Bienvenido a Litespark Landing!</p>");
+  }, []);
+
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Litespark Landing Page</h1>
+      <div dangerouslySetInnerHTML={{ __html: data }} />
+    </div>
+  );
+}
 
