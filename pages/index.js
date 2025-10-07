ad6324833b2e804714418da6bf08ddc16b427d02
Oscar Checa <ochecaa21@gmail.com>
2025-10-06 19:29:47 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+// pages/index.js
 
 export async function getServerSideProps() {
-  const res = await fetch('http://20.81.154.141:80/');
+  const res = await fetch('http://20.81.154.141:80/api/data');
   const data = await res.json();
 
   return {
@@ -14,17 +15,37 @@ export default function Home({ data }) {
   return (
     <div>
       <h1>Landing Page</h1>
+
       <h2>Categorías</h2>
       <ul>
         {data.categorias.map(cat => (
           <li key={cat.categoria_id}>{cat.nombre}</li>
         ))}
       </ul>
+
+      <h2>Proveedores</h2>
+      <ul>
+        {data.proveedores.map(prov => (
+          <li key={prov.proveedor_id}>{prov.nombre}</li>
+        ))}
+      </ul>
+
+      <h2>Clientes</h2>
+      <ul>
+        {data.clientes.map(cli => (
+          <li key={cli.cliente_id}>{cli.nombres} {cli.apellidos}</li>
+        ))}
+      </ul>
+
+      <h2>Productos</h2>
+      <ul>
+        {data.productos.map(prod => (
+          <li key={prod.producto_id}>{prod.nombre} - ${prod.precio}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-
 
 
 
